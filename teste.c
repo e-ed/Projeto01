48e4ddc5fc13758e63b653643593d7c8363b236a
rifersouli <rifersouli1@gmail.com>
2022-04-20 11:39:18 -0300

    Base

--- teste.c
+++ teste.c
@@ -1,16 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int *array, size;
+
+void printArray(int *array, int size);
+
 int main() {
 
-    printf("Faca uma piramide de asteriscos:\n\n");
-    
-    printf("*\n");
-    printf("**\n");
-    printf("***\n");
-    printf("****\n");
-    printf("*****\n");
-    printf("******\n");
+    printf("---Projeto 01 - 3SUM---\n\n");
+
+    //Objetivo: Encontrar quantas triplas únicas do array A que se obtém a soma igual a Zero!
+
+    printf("Definir o tamanho do array: ");
+    scanf(" %d", &size);
+
+    array = (int*)malloc(size * (sizeof(int)));
+
+    for(int i = 0; i < size; i++){
+        printf("Entre com o %d elemento do array: ", i+1);
+        scanf("%d", &array[i]);
+    }
+
+    printArray(array, size);
 
     return 0;
+}
+
+void printArray(int *array, int size){
+    
+    for(int i = 0; i < size; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
 }
